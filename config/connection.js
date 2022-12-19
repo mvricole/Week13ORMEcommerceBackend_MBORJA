05681eb6ce332b14a5241078968b065f192d6b73
mvricole <marian.borja@gmail.com>
2022-12-18 17:44:47 -0800

    starter code part 1.

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+// This allows sequelize to make the connection between user's login information and the database. 
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+-- Please run these commands in your mySQL shell prior to beginning the application!
+
+DROP DATABASE IF EXISTS ecommerce_db;
+
+CREATE DATABASE ecommerce_db;
