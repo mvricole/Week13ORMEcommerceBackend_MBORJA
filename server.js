7d14d66cd8ca4295866a25bf653d431196aa6a2a
mvricole <marian.borja@gmail.com>
2022-12-18 17:58:04 -0800

    added tag column definitions

---
+++ server.js
@@ -0,0 +1,19 @@
+// Declare constants to ensure application requires express and sequelize. 
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
